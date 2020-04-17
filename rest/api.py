e503da0669910a06e87dd1b026c83bbc43f6f7c0
Matteo <matteo.senese@gmail.com>
2020-04-17 15:39:26 +0200

    Changelog:  - integrated regex for new entities detection

--- rest/api.py
+++ rest/api.py
@@ -7,6 +7,7 @@ import sys
 import time
 
 from utils.preprocessing import NERSeparatePunctuations
+from utils.ner import make_ner_dict, find_from_regex
 from flask import Flask, abort, jsonify, make_response, request
 from flask_cors import CORS
 from simpletransformers.ner.ner_model import NERModel
@@ -21,113 +22,61 @@ logging.basicConfig(level=logging.INFO, filename='log/ner.log')
 model_dict = {'NERmodel': None}
 
 
-_TARGET_TO_LABEL = {'O': 0,
-                    'B-PER': 1,
-                    'I-PER': 2,
-                    'B-LOC': 3,
-                    'I-LOC': 4,
-                    'B-ORG': 5,
-                    'I-ORG': 6,
-                    'B-MISC': 7,
-                    'I-MISC': 8
-                    }
-_LABEL_TO_TARGET = ['O', 'B-PER', 'I-PER', 'B-LOC', 'I-LOC', 'B-ORG', 'I-ORG', 'B-MISC', 'I-MISC']
-
-_SHORT_TO_TYPE = {'PER': 'PERSON',
-                'LOC': 'LOCATION',
-                'ORG': 'ORGANIZATION',
-                'MISC': 'MISCELLANEOUS'
-                }
-
-# curl -i -H "Content-Type: application/json" -X POST -d '{"strings": ["Transner non gestisce <l'>: l'Europa oppure Europa. Oms trattata come persona: Oms"]}' http://localhost:5000/ner_api/v0.1/ner
-
-# curl -i -H "Content-Type: application/json" -X POST -d '{"strings": ["Vincenzo G. Fonzi è nato a Caserta il 13/08/1983", "Il seguente documento è firmato in calce per il signor Di Marzio.", "Conferma di avvenuto pagamento a Poste Italiane da parte del sig. Giuseppe Maria Boccardi."]}' http://localhost:5000/ner_api/v0.1/ner
-# curl -i -H "Content-Type: application/json" -X POST -d '{"strings": ["Maria Santos è nata a Cardenas il 13/08/1983", "The following documents were signed by John Stewart at Berlin headquarters of Deutsche Bank", "Bevestiging van betaling aan ABN AMRO door dhr. Rutger Verhoeven."]}' http://localhost:5000/ner_api/v0.1/ner
-# curl -i -H "Content-Type: application/json" -X POST -d '{"strings": ["Il sig. Francesco Antonio Di Marco lavora presso la sede centrale di Enel a Roma", "Ms. Deschamps owns a manor on the Alps in Switzerland", "David Rutte heeft twee rekeningen bij de Rabobank"]}' http://localhost:5000/ner_api/v0.1/ner
 
 
-@app.route('/ner_api/v0.1/ner', methods=['POST'])
+@app.route('/ner_api/v0.3/ner', methods=['POST'])
 def ner():
+    """This API returns the extracted entities for each sentence.
+    
+    Returns:
+        [list] -- list of dictionaries containing, for each input string, the list of entities with type, value and offset.
+        e.g.
+        {'sentence': 'Mr. Robinson lives in Reeds.'
+        'entities':[
+            {
+                'offset': 0,
+                'type': PER,
+                'value': Mr. Robinson
+            },
+            {
+                'offset': 22,
+                'type': LOC,
+                'value': Reeds 
+            }
+        ]} 
+    """
     start = time.time()
+
     raw_input_strings = request.get_json()['strings']
+
     model = model_dict['NERmodel']
+
+    # define the preprocesser to use and apply it
     preprocesser = NERSeparatePunctuations()
     input_strings = preprocesser.preprocess(raw_input_strings)
 
+    # use the trained model to extract the PER, LOC, ORG, MISC entity types
     predictions, _ = model.predict(input_strings)
-
     assert len(predictions) == len(input_strings)
-    results = []
-    curr_res = {}
-    for s, prediction in zip(input_strings, predictions):
-        curr_res = dict()
-        curr_res['sentence'] = s
-        curr_res['entities'] = []
-        curr_offset = 0
-        #for multi-words entities
-        beginning_offset = None
-        active_e_type = None
-        active_e_value = ''
 
-        for e_pred in prediction:
-            kv_pair = list(e_pred.items())
-            assert len(kv_pair) == 1
+    # retrieve the dictionary with with sentences and entities 
+    ner_dict = make_ner_dict(input_strings, predictions)
 
-            e_value, e_type = kv_pair[0]
-            #pdb.set_trace()
-            if e_type[0] == 'B':
-                #if a entity is still active, close it
-                if active_e_type:
-                    curr_entity = {'type': _SHORT_TO_TYPE[active_e_type], 'value': active_e_value[:-1], 'offset': beginning_offset}
-                    curr_res['entities'].append(curr_entity)
-                    active_e_value = ''
-                beginning_offset = curr_offset
-                active_e_type= e_type[2:]
-                active_e_value += e_value + ' ' #! active_e_value += re.sub(r'[^\w\s]', '', e_value)
-            elif e_type[0] == 'I':
-                #treat it as a beginner if the beginner is not present
-                if not active_e_type:
-                    beginning_offset = curr_offset
-                    active_e_type = e_type[2:]
-                    active_e_value += e_value + ' ' #! active_e_value += re.sub(r'[^\w\s]', '', e_value)
-                elif e_type[2:] == active_e_type:
-                    active_e_value += e_value + ' ' #! active_e_value += re.sub(r'[^\w\s]', '', e_value)
-                else:
-                    curr_entity = {'type': _SHORT_TO_TYPE[active_e_type], 'value': active_e_value[:-1], 'offset': beginning_offset}
-                    curr_res['entities'].append(curr_entity)
-                    beginning_offset = curr_offset
-                    active_e_type = e_type[2:]
-                    active_e_value += e_value + ' ' #! active_e_value += re.sub(r'[^\w\s]', '', e_value)
-            elif e_type[0] == 'O' and active_e_type:
-                curr_entity = {'type': _SHORT_TO_TYPE[active_e_type], 'value': active_e_value[:-1], 'offset': beginning_offset}
-                curr_res['entities'].append(curr_entity)
-                beginning_offset = None
-                active_e_type = None
-                active_e_value = ''
-
-            #offset takes into account also the space
-            curr_offset += len(e_value) + 1
-            #pdb.set_trace()
-            #if last prediction for that string, then save the active entities
-            if curr_offset >= len(s) and active_e_type:
-                curr_entity = {'type': _SHORT_TO_TYPE[active_e_type], 'value': active_e_value[:-1], 'offset': beginning_offset}
-                curr_res['entities'].append(curr_entity)
-        results.append(curr_res)
-
-    #pdb.set_trace()
-    #get original strings (no preprocessed) and adjust the entities offset
-    preprocesser.adjustEntitiesOffset([r['entities'] for r in results])
-    for r, original in zip(results, raw_input_strings):
+    # get original strings (no preprocessed) and adjust the entities offset
+    preprocesser.adjustEntitiesOffset([r['entities'] for r in ner_dict])
+    for r, original in zip(ner_dict, raw_input_strings):
         r['sentence'] = original
-        #r['entities'] = e
 
-    #pdb.set_trace() #* debug
+    # extract other entities by using regex
+    ner_dict = find_from_regex(ner_dict)
+
     end = time.time()
     execution_t = end-start
 
-    logging.info("-----\nUser ip: {}\nTimestamp: {}\nResponse time: {:5.2f}s\nInput strings: {}\nResponse: {}\n-----".format(request.remote_addr, start, execution_t, input_strings, results))
+    logging.info("-----\nUser ip: {}\nTimestamp: {}\nResponse time: {:5.2f}s\nInput strings: {}\nResponse: {}\n-----"
+                        .format(request.remote_addr, start, execution_t, input_strings, ner_dict))
 
-    return jsonify(results), 200
+    return jsonify(ner_dict), 200
 
 
 
@@ -138,5 +87,5 @@ def not_found(error):
 
 if __name__ == '__main__':
 
-    model_dict['NERmodel'] = NERModel('bert', SetupParameters.IT_MODEL, use_cuda= False, args={'no_cache': True, 'use_cached_eval_features': False, 'process_count': 1, 'silent': True})
+    model_dict['NERmodel'] = NERModel('bert', SetupParameters.IT_MODEL, use_cuda=False, args={'no_cache': True, 'use_cached_eval_features': False, 'process_count': 1, 'silent': True})
     app.run(host='0.0.0.0', debug=True, port=5000)
---
+++ rest/utils/ner.py
@@ -0,0 +1,136 @@
+import pdb
+import re
+
+_TARGET_TO_LABEL = {'O': 0,
+                    'B-PER': 1,
+                    'I-PER': 2,
+                    'B-LOC': 3,
+                    'I-LOC': 4,
+                    'B-ORG': 5,
+                    'I-ORG': 6,
+                    'B-MISC': 7,
+                    'I-MISC': 8
+                    }
+_LABEL_TO_TARGET = ['O', 'B-PER', 'I-PER', 'B-LOC', 'I-LOC', 'B-ORG', 'I-ORG', 'B-MISC', 'I-MISC']
+
+_SHORT_TO_TYPE = {'PER': 'PERSON',
+                'LOC': 'LOCATION',
+                'ORG': 'ORGANIZATION',
+                'MISC': 'MISCELLANEOUS'
+                }
+
+########################################################################################################################################################
+# List of regex to apply.
+# The first two regex ensures that no substrings are matched, only strings starting with space, end of the sentence or final punctuations and  ending with the same.
+# An example is a phone number like '1234567890', inside this also the NL_CITIZEN_SERVICE_NUMBER '123456789'could be matched otherwise.
+####################################
+_CLEAN_START_REGEX = '(\s|^|[.,:])' 
+_CLEAN_END_REGEX = '(\s|$|[.,])'
+_REGEX_PATTERNS = {'IT_FISCAL_CODE': _CLEAN_START_REGEX + '[A-Z]{6}[0-9]{2}[A-E,H,L,M,P,R-T][0-9]{2}[A-Z0-9]{5}' + _CLEAN_END_REGEX,
+                    'EU_IBAN': _CLEAN_START_REGEX + '[A-Z]{2}?[ ]?[0-9]{2}[]?[0-9]{4}[ ]?[0-9]{4}[ ]?[0-9]{4}[ ]?[0-9]{4}[ ]?[0-9]{4}' + _CLEAN_END_REGEX,
+                    'NL_CITIZEN_SERVICE_NUMBER': _CLEAN_START_REGEX + '[0-9]{9}' + _CLEAN_END_REGEX ,
+                    'UK_NATIONAL_ID_NUMBER': _CLEAN_START_REGEX + '[A-CEGHJ-PR-TW-Z][A-CEGHJ-NPR-TW-Z]{1}[0-9]{6}[A-DFM]?' + _CLEAN_END_REGEX,
+                    'EU_PHONE_NUMBER': _CLEAN_START_REGEX + '([+]*[(]?[0-9]{1,4}[)]?){0,1}([-\s\./0-9]+){10}' + _CLEAN_END_REGEX,
+                    'EMAIL_ADDRESS': _CLEAN_START_REGEX + '[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+' + _CLEAN_END_REGEX,
+                    'IPV4_ADDRESS': _CLEAN_START_REGEX + '((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}' + _CLEAN_END_REGEX
+                }
+
+
+def make_ner_dict(input_strings, predictions):
+    """This function produces a dictionary for the entities in predictions with the format {'sentence': ..., 'entities' :[{'type': , 'value': , 'offset':}]}
+    
+    Arguments:
+        input_strings {list} -- list of the string from where the predictions come
+        predictions {list} -- list of entities predicted for each input strings
+    
+    Returns:
+        list -- list of dictionaries having the shape {'sentence': ..., 'entities' :[...]}
+    """
+    result_dict = []
+    curr_res = {}
+    for s, prediction in zip(input_strings, predictions):
+
+        curr_res = dict()
+
+        curr_res['sentence'] = s
+        curr_res['entities'] = []
+        curr_offset = 0
+        #for multi-words entities
+        beginning_offset = None
+        active_e_type = None
+        active_e_value = ''
+
+        for e_pred in prediction:
+            kv_pair = list(e_pred.items())
+            assert len(kv_pair) == 1
+
+            e_value, e_type = kv_pair[0]
+            
+            if e_type[0] == 'B':
+                #if a entity is still active, close it
+                if active_e_type:
+                    curr_entity = {'type': _SHORT_TO_TYPE[active_e_type], 'value': active_e_value[:-1], 'offset': beginning_offset}
+                    curr_res['entities'].append(curr_entity)
+                    active_e_value = ''
+                beginning_offset = curr_offset
+                active_e_type= e_type[2:]
+                active_e_value += e_value + ' ' #! active_e_value += re.sub(r'[^\w\s]', '', e_value)
+            elif e_type[0] == 'I':
+                #treat it as a beginner if the beginner is not present
+                if not active_e_type:
+                    beginning_offset = curr_offset
+                    active_e_type = e_type[2:]
+                    active_e_value += e_value + ' ' #! active_e_value += re.sub(r'[^\w\s]', '', e_value)
+                elif e_type[2:] == active_e_type:
+                    active_e_value += e_value + ' ' #! active_e_value += re.sub(r'[^\w\s]', '', e_value)
+                else:
+                    curr_entity = {'type': _SHORT_TO_TYPE[active_e_type], 'value': active_e_value[:-1], 'offset': beginning_offset}
+                    curr_res['entities'].append(curr_entity)
+                    beginning_offset = curr_offset
+                    active_e_type = e_type[2:]
+                    active_e_value += e_value + ' ' #! active_e_value += re.sub(r'[^\w\s]', '', e_value)
+            elif e_type[0] == 'O' and active_e_type:
+                curr_entity = {'type': _SHORT_TO_TYPE[active_e_type], 'value': active_e_value[:-1], 'offset': beginning_offset}
+                curr_res['entities'].append(curr_entity)
+                beginning_offset = None
+                active_e_type = None
+                active_e_value = ''
+
+            #offset takes into account also the space
+            curr_offset += len(e_value) + 1
+            #pdb.set_trace()
+            #if last prediction for that string, then save the active entities
+            if curr_offset >= len(s) and active_e_type:
+                curr_entity = {'type': _SHORT_TO_TYPE[active_e_type], 'value': active_e_value[:-1], 'offset': beginning_offset}
+                curr_res['entities'].append(curr_entity)
+        result_dict.append(curr_res)
+
+    return result_dict
+
+
+
+
+
+def find_from_regex(ner_dict):
+    """Find matches of regex patterns from ner_dict and insert the new entities found by means of the regex.
+    For each string only the first occurence of each regex is returned.
+    
+    Arguments:
+        input_strings {list} -- list of strings to analyze
+    """
+
+    for item in ner_dict:
+        for field, regex in _REGEX_PATTERNS.items():
+            match = re.search(regex, item['sentence'])
+            if match:
+                matched_string = match.group(0)
+                # remove initial or final space/punctuation if it was catched by regex
+                if matched_string[0] in '.,: ':
+                    matched_string = matched_string[1:]
+                if matched_string[-1] in '., ':
+                    matched_string = matched_string[:-1]
+                span = match.span(0)
+                item['entities'].append({'type': field, 'value': matched_string, 'offset': span[0]})
+    
+    return ner_dict
+
