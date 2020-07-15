28c02b647fbdccc7b111365abbcf9297d2f48a3b
Matteo <matteo.senese@gmail.com>
2020-07-15 12:28:49 +0200

    Argparse to read from cmdline

--- wikiner_tools/stats/stats.py
+++ wikiner_tools/stats/stats.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader, Dataset
 
 class WikiNerCoNLL(Dataset):
 
-    def __init__(self, path):
+    def __init__(self, path, datatype):
         """Class for statistics on WikiNER
 
         Args:
@@ -17,9 +17,12 @@ class WikiNerCoNLL(Dataset):
         self.targets = []
         self.sentences = []
 
-        self.readfile(path, 'train')
-        self.readfile(path, 'val')
-        self.readfile(path, 'test')
+        if datatype is not None:
+            self.readfile(path, datatype)
+        else:
+            self.readfile(path, 'train')
+            self.readfile(path, 'val')
+            self.readfile(path, 'test')
         assert len(self.words) == len(self.targets)
 
     def readfile(self, path, datatype='train'):
@@ -36,10 +39,10 @@ class WikiNerCoNLL(Dataset):
             self.sentences[-1].append(self.words[-1])
 
     def __getitem__(self, index):
-        t = self.targets[index]
-        if len(t) > 3 and t != 'MISC':
-            t = t[2:]
-        return self.words[index], t
+        #t = self.targets[index]
+        #if len(t) > 3 and t != 'MISC':
+        #    t = t[2:]
+        return self.words[index], self.targets[index]
 
     def __len__(self):
         return len(self.words)
@@ -60,20 +63,28 @@ if __name__ == '__main__':
         required=True,
         help='Path to wikiner in conll format folder'
     )
+    parser.add_argument(
+        '--type',
+        default=None,
+        type=str,
+        required=False,
+        help='train, val or test (optional: if not specified all of them are considered)'
+    )
 
     args = parser.parse_args()
 
-    dataset = WikiNerCoNLL(args.data)
+    dataset = WikiNerCoNLL(args.data, args.type)
     loader = DataLoader(dataset)
     
     # stats data structures
     unique_words = set()
-    num_entities_per_type = {'PER': 0, 'LOC': 0, 'ORG': 0, 'MISC': 0, 'O': 0}
+    num_entities_per_type = {'B-PER': 0, 'B-LOC': 0, 'B-ORG': 0, 'B-MISC': 0, 'O': 0}
     for (word, target) in loader:
         w = word[0]
         t = target[0]
 
-        num_entities_per_type[t] += 1
+        if t[0] == 'B' or t == 'O':
+            num_entities_per_type[t] += 1
         if w not in string.punctuation:
             unique_words.add(word)
 
--- wikiner_tools/wikiNER2CoNLL.py
+++ wikiner_tools/wikiNER2CoNLL.py
@@ -1,5 +1,5 @@
 import pdb
-import getopt
+import argparse
 import sys
 
 import numpy as np
@@ -33,81 +33,101 @@ def adjust_sizes(train_size, val_size, test_size):
         return offset + 1
 
 
-# python wikiNER2CoNLL.py -o wikiner_conll/it/wikinerIT.conll -i wikiner_original/aijwikineritwp2 -v .15 -t .7 -e .15 --augment
+
 
 if __name__ == '__main__':
+    """Example
+        python wikiNER2CoNLL.py --infile wikiner_original/aijwikineritwp2\
+                                --outfile wikiner_conll/it/wikinerIT.conll\
+                                --trainsplit .7\
+                                --valsplit .15\
+                                --testsplit .15\
+                                --augment
 
-    infile = ''
-    outfile = ''
-    val_split = 0
-    train_split = 0
-    test_split = 0
-    argv = sys.argv[1:]
-    rand_split = False
-    augment = False
-    try:
-        opts, args = getopt.getopt(argv,"hi:o:v:t:e:ra",["help=", "input=", "output=", "validation=", "training=", "test=", "randomsplit="])
-    except getopt.GetoptError:
-        print ('wikiNER2CoNLL.py -i <input_file> -o <output_file>')
-        sys.exit(2)
-    for count, (opt, arg) in enumerate(opts):
-        if opt in ('-h', '--help'):
-            print ('Correct format: wikiNER2CoNLL.py -i <input_file> -o <output_file>')
-            sys.exit()
-        elif opt in ("-i", "--input"):
-            infile = arg
-        elif opt in ("-o", "--output"):
-            outfile = arg
-        elif opt in ("-v", "--validation"):
-            val_split = float(arg)
-        elif opt in ("-t", "--training"):
-            train_split = float(arg)
-        elif opt in ("-e", "--test"):
-            test_split = float(arg)
-        elif opt in ("-r", "--randomsplit"):
-            rand_split = True
-        elif opt in ("-a", "--augment"):
-            augment = True
-        else:
-            print('Uncorrect format (-h for help)')
-            sys.exit(2)
-    
-    if len(infile) == 0 or len(outfile) == 0:
+    """
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(        
+        "--infile",
+        default=None,
+        type=str,
+        required=True,
+        help="Path to input file")
+    parser.add_argument(        
+        "--outfile",
+        default=None,
+        type=str,
+        required=True,
+        help="Path to output file")
+    parser.add_argument(        
+        "--valsplit",
+        default=None,
+        type=float,
+        required=True,
+        help="Split ratio for validation")
+    parser.add_argument(        
+        "--trainsplit",
+        default=None,
+        type=float,
+        required=True,
+        help="Split ratio for training")
+    parser.add_argument(        
+        "--testsplit",
+        default=None,
+        type=float,
+        required=True,
+        help="Split ratio for test")
+    parser.add_argument(        
+        "--randsplit",
+        default=False,
+        action='store_true',
+        required=False,
+        help="To apply random split")
+    parser.add_argument(        
+        "--augment",
+        default=False,
+        action='store_true',
+        required=False,
+        help="To apply data augmentation")
+
+    args = parser.parse_args()
+
+    if len(args.infile) == 0 or len(args.outfile) == 0:
         print('Missing arguments')
         exit(2)
-    if val_split < 0 or val_split > 1 or train_split < 0 or train_split > 1 or test_split < 0 or test_split > 1:
+    if args.valsplit < 0 or args.valsplit > 1 or args.trainsplit < 0 or args.trainsplit > 1 or args.testsplit < 0 or args.testsplit > 1:
         print('The training, validation and test factors must in the range [0,1]')
         exit(2)
-    if not (val_split + test_split + train_split == 0 or val_split + test_split + train_split == 1):
+    if not (args.valsplit + args.testsplit + args.trainsplit == 0 or args.valsplit + args.testsplit + args.trainsplit == 1):
         print('The sum of training, validation and test factors must be 1')
         exit(2)
-    #inputfile = r'./wikinerIT'
-    #outfile = r'./wikiNER.conll'
-    dataset = WikiNER(infile)
+
+    dataset = WikiNER(args.infile)
     
-    if val_split + test_split + train_split == 1:
+    if args.valsplit + args.testsplit + args.trainsplit == 1:
         # create train, validation and test sets
-        train_size = int(np.floor(train_split * dataset.__len__()))
-        val_size = int(np.floor(val_split * dataset.__len__()))
-        test_size = int(np.floor(test_split * dataset.__len__()))
+        train_size = int(np.floor(args.trainsplit * dataset.__len__()))
+        val_size = int(np.floor(args.valsplit * dataset.__len__()))
+        test_size = int(np.floor(args.testsplit * dataset.__len__()))
         # it can happen that the dataset is not perfectly divisible
         train_size += adjust_sizes(train_size, val_size, test_size)
         
-
-        if rand_split:
+        pdb.set_trace()
+        if args.randsplit:
             train_set, val_set, test_set = random_split(dataset, [train_size, val_size, test_size])
         else:
             indices = list(range(len(dataset)))
             train_set = Subset(dataset, indices[:train_size])
             val_set = Subset(dataset, indices[train_size: train_size+val_size])
             test_set = Subset(dataset, indices[train_size+val_size:])
-            
-        wikiNER2CoNLL(train_set, outfile+'.train', augment=augment)
-        wikiNER2CoNLL(val_set, outfile+'.val', augment=augment)
-        wikiNER2CoNLL(test_set, outfile+'.test', augment=augment)
 
-        print('Output files: '+outfile.split('/')[-1]+'.train'+', '+outfile.split('/')[-1]+'.val'+', '+outfile.split('/')[-1]+'.test')
+        wikiNER2CoNLL(train_set, args.outfile+'.train', augment=args.augment)
+        wikiNER2CoNLL(val_set, args.outfile+'.val', augment=args.augment)
+        wikiNER2CoNLL(test_set, args.outfile+'.test', augment=args.augment)
+
+        print('Output files: '+args.outfile.split('/')[-1]+'.train'+', '+args.outfile.split('/')[-1]+'.val'+', '+args.outfile.split('/')[-1]+'.test')
 
     else:
-        wikiNER2CoNLL(dataset, outfile, augment=augment)
-        print('Output file: '+outfile.split('/')[-1])
+        wikiNER2CoNLL(dataset, args.outfile, augment=args.augment)
+        print('Output file: '+args.outfile.split('/')[-1])
